fe1e3a19e539e416a5f9499bf9b6a0d9f015400d
Srikalyan Megati <kalmegati@gmail.com>
2020-02-07 16:57:42 +0000

    Automatically backed up by Learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,41 @@
+class SongsController < ApplicationController
+  
+  def new
+    @song = Song.new
+  end
+  
+  def create
+    @song = Song.create(song_params(:name))
+    redirect_to song_path(@song)
+  end
+
+  def edit
+    @song = song_find
+  end
+  
+  def update
+    @song = song_find
+    @song.update(song_params(:name))
+    redirect_to song_path
+  end
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    byebug
+    @song = song_find
+  end
+  
+  private
+  
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
+  
+  def song_find
+    Song.find(params[:id])
+  end
+  
+end
